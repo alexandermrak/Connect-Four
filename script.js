3638ccacf97eca19070b66e8bda2bd14ce62fdd2
alexandermak <alexandermrak23@gmail.com>
2021-03-24 11:19:30 -0500

    Work from monday

---
+++ script.js
@@ -0,0 +1,25 @@
+//constants
+// things that will not change
+
+//variables
+// things that will change
+
+//cached element references
+// manipulate html
+
+//event listeners
+// this will be each column button to drop the token into
+
+//functions
+
+function initialize() {
+
+}
+
+function dropToken() {
+
+}
+
+function render() {
+
+}
